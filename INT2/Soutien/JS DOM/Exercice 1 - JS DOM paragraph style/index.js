f0c9377d48bb10b1c17efe05b91d0a40db095ae9
Joris-Guesne <github.r40ui@passmail.net>
2024-10-31 17:20:11 +0100

    soutien INT2

--- INT2/Soutien/JS DOM/Exercice 1 - JS DOM paragraph style/index.js
+++ INT2/Soutien/JS DOM/Exercice 1 - JS DOM paragraph style/index.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
   // Au clic sur le bouton ayant la classe js-submit-button
   // Changer la taille, famille et couleur de la police
   // de l'élément ayant la classe js-text
+
+  
+  let buttonDOM = document.querySelector('.js-submit-button');
+
+  buttonDOM.addEventListener('click', function(){
+    
+    let textDOM = document.querySelector('.js-text');
+    textDOM.style.fontFamily = 'monospace';
+    textDOM.style.color = 'blue';
+    textDOM.style.fontSize = '5rem';
+  });
 });
