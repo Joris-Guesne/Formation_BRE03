fe91a5a7321cecdabbd2c950a024c02e2fb73f24
Joris Guesné <joris.dev@proton.me>
2024-12-17 10:31:08 +0100

    Ajout de l'exercice require

---
+++ PHP/php-j2/Exercice require/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$siteTitle = "J'aime pas les lundis";
+$pageTitle = "Qui est Garfield ?";
+$pageContent = "Un chat roux qui aime les lasagnes.";
+
+require_once(__DIR__ . '/templates/layout.phtml');
