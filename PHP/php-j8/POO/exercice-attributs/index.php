2e09cabf5fa6380752f45d5428f0ed4afc7de0db
Joris-Guesne <joris.dev@proton.me>
2025-01-19 09:34:12 +0100

    test

--- PHP/php-j8/POO/exercice-attributs/index.php
+++ PHP/php-j8/POO/exercice-attributs/index.php
@@ -8,7 +8,7 @@ $sarah = new Character(1);
 
 echo ($sarah->getFullName());
 
-$sarah->setFirstName("Sarah");
+            $sarah->setFirstName("Sarah");
 $sarah->setLastName("Connor");
 
 echo ($sarah->getFullName());
