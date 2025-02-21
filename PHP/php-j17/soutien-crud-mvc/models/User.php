695c69479dffa734658482f72ff07236c03f5b5f
Joris-Guesne <joris.dev@proton.me>
2025-02-21 17:26:01 +0100

    🐛 Ajout du setter getter de l'id

--- PHP/php-j17/soutien-crud-mvc/models/User.php
+++ PHP/php-j17/soutien-crud-mvc/models/User.php
@@ -70,4 +70,22 @@ class User
 
         return $this;
     }
+
+    /**
+     * Get the value of id
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
 }
